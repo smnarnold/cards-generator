fb754ec8a83cb94a9d0b6b680ca38dc6f01ad961
Maxime Daoust <mdaoust@sidlee.com>
2017-03-15 11:51:37 -0400

    - reformat

--- _frontend/src/js/boot.js
+++ _frontend/src/js/boot.js
@@ -21,28 +21,28 @@ window.initClasses = function (context) {
     var pages = context[0].querySelectorAll('[data-page]');
     var modules = context[0].querySelectorAll('[data-module]');
 
-    for(var x=0; x<pages.length; x++) {
+    for (var x = 0; x < pages.length; x++) {
         var page = pages[x];
         var pagesList = page.getAttribute('data-page').split(/\s+/);
 
-        for(var y=0; y<pagesList.length; y++) {
+        for (var y = 0; y < pagesList.length; y++) {
             var pageName = pagesList[y];
-            
+
             if (Pages[pageName] !== undefined) {
-                new Pages[pageName]( $(page) ).init();
+                new Pages[pageName]($(page)).init();
             }
         }
     }
 
-    for(var i=0; i<modules.length; i++) {
+    for (var i = 0; i < modules.length; i++) {
         var module = modules[i];
         var modulesList = module.getAttribute('data-module').split(/\s+/);
 
-        for(var j=0; j<modulesList.length; j++) {
+        for (var j = 0; j < modulesList.length; j++) {
             var moduleName = modulesList[j];
 
             if (Modules[moduleName] !== undefined) {
-                new Modules[moduleName]( $(module) ).init();
+                new Modules[moduleName]($(module)).init();
             }
         }
     }

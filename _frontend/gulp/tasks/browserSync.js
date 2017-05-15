081e58ea11ba455a65598aa9b472571d278a5b3d
Maxime Daoust <mdaoust@sidlee.com>
2017-05-15 13:56:20 -0400

    - proxy comment added

--- _frontend/gulp/tasks/browserSync.js
+++ _frontend/gulp/tasks/browserSync.js
@@ -4,6 +4,7 @@ var browserSync = require('browser-sync');
 var browserSyncTask = function() {
   browserSync.init({
       open: false,
+      // proxy: "dev.local",
       server: {
           baseDir: '../'
       },

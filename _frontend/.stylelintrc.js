e6ef3bd519184f911e43871030a4f7c7ff535ca7
Maxime Daoust <mdaoust@sidlee.com>
2018-03-09 16:38:23 -0500

    - stylelint now inore colors that are inside a function.

--- _frontend/.stylelintrc.js
+++ _frontend/.stylelintrc.js
@@ -4,6 +4,9 @@ module.exports = {
     "stylelint-config-sass-guidelines",
   ],
   "rules": {
+    "color-named": ["never", {
+      "ignore": ["inside-function"]
+    }],
     "at-rule-no-unknown": null,
     "max-nesting-depth": 3,
   },

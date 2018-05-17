35922aad5fbc32051a9b481211c5ec99b90295d8
Maxime Daoust <mdaoust@sidlee.com>
2018-05-17 15:39:48 -0400

    - BEM selector pattern support added

--- _frontend/.stylelintrc.js
+++ _frontend/.stylelintrc.js
@@ -9,5 +9,6 @@ module.exports = {
     }],
     "at-rule-no-unknown": null,
     "max-nesting-depth": 3,
+    "selector-class-pattern": "^(?:(?:o|c|u|t|s|is|has|_|js|qa)-)?[a-zA-Z0-9]+(?:-[a-zA-Z0-9]+)*(?:__[a-zA-Z0-9]+(?:-[a-zA-Z0-9]+)*)?(?:--[a-zA-Z0-9]+(?:-[a-zA-Z0-9]+)*)?(?:\\[.+\\])?$",
   },
 };

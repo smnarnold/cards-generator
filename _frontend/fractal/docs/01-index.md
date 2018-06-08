32efe62fb42e80cbd4fd57c7907dfb6557ee712a
Maxime Daoust <mdaoust@sidlee.com>
2018-06-08 15:51:43 -0400

    - gitignore cleanup - fractal cleanup

--- _frontend/fractal/docs/01-index.md
+++ _frontend/fractal/docs/01-index.md
@@ -1,5 +1,5 @@
 ---
-title: Demo
+title: Overview
 ---
 
 This is your index page. You can edit its contents at `docs/01-index.hbs`
--- _frontend/fractalfile.js
+++ _frontend/fractalfile.js
@@ -2,58 +2,31 @@
 // ref: https://github.com/frctl/fractal/issues/118#issuecomment-255254117
 'use strict';
 
-/*
-* Require the path module
-*/
 const path = require('path');
-
-/*
- * Require the Fractal module
- */
-const fractal = module.exports = require('@frctl/fractal').create();
-
-/*
- * Require the Mandelbrot theme module (default)
- */
+const fractal = module.exports = require('@frctl/fractal').create(); // Create a new Fractal instance and export it for use elsewhere if required
 const mandelbrot = require('@frctl/mandelbrot'); // require the Mandelbrot theme module
 
+// create a new instance with custom config options
+const sidleeTheme = mandelbrot({
+  nav: ["docs", "components"],
+  skin: "black",
+});
 
+fractal.web.theme(sidleeTheme); // tell Fractal to use the configured theme by default
+
+/* Set the title of the project */
 fractal.set('project.title', 'Sid Lee');
 fractal.set('project.version', 'v1.0');
 fractal.set('project.author', 'Sid Lee');
 
+/* Tell Fractal where the components will live */
+fractal.components.set('path', path.join(__dirname, 'fractal/components'));
 
-/*
- * Create a new instance with custom config options
- */
-const sidLeeTheme = mandelbrot({
-    skin: "black",
-    styles: [
-    	"default"
-    ]
-    // any other theme configuration values here
-});
+/* Tell Fractal where the documentation pages will live */
+fractal.docs.set('path', path.join(__dirname, 'fractal/docs'));
 
-/*
- * Tell Fractal where to look for components.
- */
-fractal.components.set('path', path.join(__dirname, 'demo/components'));
-
-/*
- * Tell Fractal where to look for documentation pages.
- */
-fractal.docs.set('path', path.join(__dirname, 'demo/docs'));
-
-/*
- * Tell the Fractal web preview plugin where to look for static assets.
- */
+/* Specify a directory of static assets */
 fractal.web.set('static.path', path.join(__dirname, '../assets'));
 
-/*
- * Tell the Fractal where to look for dest.
- */
-fractal.web.set('builder.dest', path.join(__dirname, 'demo/build'));
-
-
-
-fractal.web.theme(sidLeeTheme); // tell Fractal to use the configured theme by default
+/* Set the static HTML build destination */
+fractal.web.set('builder.dest', path.join(__dirname, 'fractal/build'));
